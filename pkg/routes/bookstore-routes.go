633b06e8aa760d4b75f5c826d80b297b5abeba59
Cary <cbondoc.it@gmail.com>
2023-05-07 11:52:43 +0800

    routes: added create/get book get/put/delete by id

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,3 +1,15 @@
 package routes
 
+import (
+	"github.com/cbondoc/go-bookstore/pkg/controllers"
+	"github.com/gorilla/mux"
+)
 
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.Handle("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
